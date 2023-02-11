8018ff207ec82210dfd60f3cf4062920e9048157
Michael Nittolo <mnittolo3@gmail.com>
2023-02-11 12:20:10 -0500

    added list of dummy jobs and added styling

--- client/components/Jobs/JobList.js
+++ client/components/Jobs/JobList.js
@@ -1,10 +1,10 @@
 import {myJobs}from "../../public/Jobs/dummy-jobs.js";
 import JobItem from "@/components/Jobs/JobItem";
-import "../../styles/Jobs/jobs.module.css";
+import styles from"../../styles/Jobs/jobs.module.css";
 
 function JobList() {
     let jobs = myJobs();
-    return <section className="job-list">
+    return <section className={styles.joblist}>
         {jobs.map(job => <JobItem title={job.title} company={job.company} />)}
     </section>
 }
