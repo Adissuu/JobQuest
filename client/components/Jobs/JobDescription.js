1753c7a8f49950de0add6f7c5e6f70c90e77241c
Michael Nittolo <mnittolo3@gmail.com>
2023-02-11 15:05:11 -0500

    added routing to jobs/[Id] page

--- client/components/Jobs/JobDescription.js
+++ client/components/Jobs/JobDescription.js
@@ -1,6 +1,7 @@
 import { findJob } from "../../public/Jobs/dummy-jobs.js";
 import { useRouter } from "next/router.js";
 import Link from "next/link.js";
+import styles from "../../styles/Jobs/jobs.module.css";
 
 function JobDescription(props) {
     const router = useRouter();
@@ -9,7 +10,7 @@ function JobDescription(props) {
     let url;
     url="view/"+props.id;
     if (router.asPath.indexOf("view")>-1) url = props.id;
-    return <Link href={url} passHref={true}>
+    return <Link href={url} className={styles.jobdescription}>
         <div>{job.title}</div>
         <div>{job.company}</div>
         <div>{job.description}</div>
--- client/components/Jobs/JobList.js
+++ client/components/Jobs/JobList.js
@@ -8,17 +8,16 @@ function JobList() {
   const router = useRouter();
   console.log(router.asPath == "/jobs");
   let jobs = myJobs();
+  let url = "";
+  if (router.asPath=="/jobs") {
+    url = "/jobs/"
+  }
   let content;
   content = jobs.map((job) => (
-    <Link href={"jobs/"+job.id}>
+    <Link href={url+job.id}>
       <JobItem title={job.title} company={job.company} />
     </Link>
   ));
-  if (router.asPath != "/jobs") {
-    content = jobs.map((job) => (
-          <JobItem title={job.title} company={job.company} />
-      ));
-  }
   return (
     <section className={`${styles.joblist} ${styles.scrollable}`}>
       {content}
