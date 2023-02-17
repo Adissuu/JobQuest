20425d2dc1889430f37f663d380a432408b4602f
Gabriel <gabrieldubois.eng@gmail.com>
2023-02-16 23:46:54 -0500

    JQ-6 and JQ-7, finished implementation of the style of the sign in and sign up page, will need to link to backend when we can

--- client/components/Utils/SignupComponent.js
+++ client/components/Utils/SignupComponent.js
@@ -1,13 +1,16 @@
 import { useState, useEffect } from 'react';
+import Image from 'next/image'
+import signupImg from '@/public/Signin-up/Signup_img.png';
 import Router from 'next/router';
 import Link from 'next/link';
 
 const SignupComponent = () => {
     const [values, setValues] = useState({
-        name: 'Hello',
-        email: 'grmeiger',
+        name: 'User.name',
+        email: 'email@email.com',
         password: 'greomgfdlk',
-        confirmPassword: 'gmrepmfd',
+        confirmPassword: 'greomgfdlk',
+        type: "Recruiter",
         error: '',
         loading: false,
         message: '',
@@ -24,46 +27,59 @@ const SignupComponent = () => {
 
     const signupForm = () => {
         return (
-            <div className="bg-white dark:bg-green-500 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <h1 className=''>Sign In</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">Name</label>
-                        <input value={name}
-                            type="text"
-                            className="" readOnly />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">Email</label>
-                        <input value={email}
-                            type="email"
-                            className="" readOnly />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-                        <input value={password}
-                            type="Password"
-                            className="" readOnly />
-                    </div>
-                    <div className="mb-6">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">Confirm Password</label>
-                        <input value={confirmPassword}
-                            type="Password"
-                            className="" readOnly />
-                    </div>
-                    <div className='flex justify-center'>
-                        <button className="">Sign Up</button>
-                    </div>
-                </form>
-            </div>
-
+            <>
+                <div className="bg-white dark:bg-green-500 dark:text-black-100 shadow-md rounded pl-8 flex">
+                    <form onSubmit={handleSubmit} className="max-w-md mt-4">
+                        <h1 className='text-center text-2xl font-bold'>Sign up</h1>
+                        <div className="flex">
+                            <div className="mb-4">
+                                <label className="block text-gray-700 rounded text-sm font-bold mb-2">Name</label>
+                                <input value={name}
+                                    type="text"
+                                    className="dark:text-white pl-2" readOnly />
+                            </div>
+                            <div className="mb-4 ml-2">
+                                <label className="block text-gray-700 rounded text-sm font-bold mb-2">Email</label>
+                                <input value={email}
+                                    type="email"
+                                    className="dark:text-white pl-2" readOnly />
+                            </div>
+                        </div>
+                        <div className="flex">
+                            <div className="mb-4">
+                                <label className="block text-gray-700 rounded text-sm font-bold mb-2">Password</label>
+                                <input value={password}
+                                    type="Password"
+                                    className="dark:text-white pl-2" readOnly />
+                            </div>
+                            <div className="mb-6 ml-2">
+                                <label className="block text-gray-700 rounded text-sm font-bold mb-2">Confirm Password</label>
+                                <input value={confirmPassword}
+                                    type="Password"
+                                    className="dark:text-white pl-2" readOnly />
+                            </div>
+                        </div>
+                        <div className="mb-6">
+                            <label className="block text-gray-700 rounded text-sm font-bold mb-2">Choose Your Class</label>
+                            <select className="dark:text-white pl-2 w-full rounded">
+                                <option>Job Seeker</option>
+                                <option>Recruiter</option>
+                            </select>
+                        </div>
+                        <button className="bg-green-500 shadow-md hover:shadow-sm text-white font-semibold py-2 px-4 rounded w-full" type="button">
+                            Start Your Quest
+                        </button>
+                    </form>
+                    <Image src={signupImg} className="ml-4 w-80 h-80 rounded-r" alt="signup-img" />
+                </div>
+            </>
         );
     }
 
     return (
-        <>
+        <div className=''>
             {showForm && signupForm()}
-        </>
+        </div>
     )
 };
 
