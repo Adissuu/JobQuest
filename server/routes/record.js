abcde8312c21c4942b3ceb9be90e1a545fbb7163
Etienne Paquet <paquetetienne@gmail.com>
2023-02-23 00:33:31 -0500

    Remade backend from MongoDB mern model

--- server/routes/record.js
+++ server/routes/record.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
-// employerRoutes is an instance of the express router.
+// recordRoutes is an instance of the express router.
 // We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /employer.
-const employerRoutes = express.Router();
+// The router will be added as a middleware and will take control of requests starting with path /record.
+const recordRoutes = express.Router();
 
 // This will help us connect to the database
 const dbo = require("../db/conn");
@@ -12,11 +12,11 @@ const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
 
 
-// This section will help you get a list of all the employers.
-employerRoutes.route("/employer").get(function (req, res) {
+// This section will help you get a list of all the records.
+recordRoutes.route("/record").get(function (req, res) {
   let db_connect = dbo.getDb("JobQuest");
   db_connect
-    .collection("Employers")
+    .collection("records")
     .find({})
     .toArray(function (err, result) {
       if (err) throw err;
@@ -24,34 +24,34 @@ employerRoutes.route("/employer").get(function (req, res) {
     });
 });
 
-// This section will help you get a single employer by id
-employerRoutes.route("/employer/:id").get(function (req, res) {
+// This section will help you get a single record by id
+recordRoutes.route("/record/:id").get(function (req, res) {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId( req.params.id )};
   db_connect
-      .collection("Employers")
+      .collection("records")
       .findOne(myquery, function (err, result) {
         if (err) throw err;
         res.json(result);
       });
 });
 
-// This section will help you create a new employer.
-employerRoutes.route("/employer/add").post(function (req, response) {
+// This section will help you create a new record.
+recordRoutes.route("/record/add").post(function (req, response) {
   let db_connect = dbo.getDb();
   let myobj = {
     name: req.body.name,
     position: req.body.position,
     level: req.body.level,
   };
-  db_connect.collection("employers").insertOne(myobj, function (err, res) {
+  db_connect.collection("records").insertOne(myobj, function (err, res) {
     if (err) throw err;
     response.json(res);
   });
 });
 
-// This section will help you update a employer by id.
-employerRoutes.route("/update/:id").post(function (req, response) {
+// This section will help you update a record by id.
+recordRoutes.route("/update/:id").post(function (req, response) {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId( req.params.id )};
   let newvalues = {
@@ -62,7 +62,7 @@ employerRoutes.route("/update/:id").post(function (req, response) {
     },
   };
   db_connect
-    .collection("Employers")
+    .collection("records")
     .updateOne(myquery, newvalues, function (err, res) {
       if (err) throw err;
       console.log("1 document updated");
@@ -70,15 +70,15 @@ employerRoutes.route("/update/:id").post(function (req, response) {
     });
 });
 
-// This section will help you delete a employer
-employerRoutes.route("/:id").delete((req, response) => {
+// This section will help you delete a record
+recordRoutes.route("/:id").delete((req, response) => {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId( req.params.id )};
-  db_connect.collection("Employers").deleteOne(myquery, function (err, obj) {
+  db_connect.collection("records").deleteOne(myquery, function (err, obj) {
     if (err) throw err;
     console.log("1 document deleted");
     response.json(obj);
   });
 });
 
-module.exports = employerRoutes;
+module.exports = recordRoutes;
