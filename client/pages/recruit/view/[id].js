2a13e06219b68a20b68a4c72fcfbfb181e373798
The-Einzelganger <123901407+TitiMMM@users.noreply.github.com>
2023-02-28 14:42:51 -0500

    Modified recruit id page

--- client/pages/recruit/view/[id].js
+++ client/pages/recruit/view/[id].js
@@ -1,16 +1,16 @@
 import Layout from "@/components/UI/Layout";
-import JobDescription from "@/components/Jobs/JobDescription";
+import RecruitsDescription from "@/components/Recruits/RecruitsDescription";
 import { useRouter } from "next/router";
 
-const Jobs3 = () => {
+const Recruits3 = () => {
     const router = useRouter();
-    console.log(router.query.jobId)
+    console.log(router.query.recruitid)
     return (
-        <Layout pathname="jobs">
-            <h1>Some specific job page</h1>
-            <JobDescription />
+        <Layout pathname="recruit">
+            <h1>Some specific recruit's page</h1>
+            <RecruitsDescription />
         </Layout>
     )
 }
 
-export default Jobs3;
+export default Recruits3;
