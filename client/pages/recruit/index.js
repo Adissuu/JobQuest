9f9fee7feb86506dbc967dbccbb8fcc839f91b53
The-Einzelganger <titimorvan2001@gmail.com>
2023-02-27 19:30:42 -0500

    Testing git Recruits page

--- client/pages/recruit/index.js
+++ client/pages/recruit/index.js
@@ -3,7 +3,7 @@ import Layout from "@/components/UI/Layout";
 const Recruit = () => {
     return (
         <Layout pathname="recruit">
-            <h1>Recruit page</h1>
+            <h1>Recruit page 1</h1>
         </Layout>
     )
 }
