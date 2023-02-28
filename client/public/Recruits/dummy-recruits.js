fd699cdacd017c3d1852fe341c8ee58e33bd737a
The-Einzelganger <titimorvan2001@gmail.com>
2023-02-28 13:01:34 -0500

    Added dummy recruits as database for the recruits page

---
+++ client/public/Recruits/dummy-recruits.js
@@ -0,0 +1,202 @@
+const jobs = [
+ {
+  Name: "Sarah Johnson",
+  City: "Los Angeles, CA",
+  Education: "Bachelor's degree in Psychology",
+  Experience: "3 years of experience as a counselor in a mental health clinic",
+ },  
+ {
+  Name: "David Lee",
+  City: "New York, NY",
+  Education: "Master's degree in Finance",
+  Experience: "5 years of experience as a financial analyst at a large investment bank",
+ },
+ {
+  Name: "Emily Chen",
+  City: "San Francisco, CA",
+  Education: "Bachelor's degree in Computer Science",
+  Experience: "2 years of experience as a software engineer at a tech startup",
+ },
+ {
+  Name: "Michael Davis",
+  City: "Chicago, IL",
+  Education: "Master's degree in Business Administration",
+  Experience: "6 years of experience as a project manager in the construction industry",
+ },
+ {
+  Name: "Samantha Brown",
+  City: "Miami, FL",
+  Education: "Bachelor's degree in Marketing",
+  Experience: "4 years of experience as a marketing specialist at a consumer goods company",
+ },
+ {
+  Name: "John Smith",
+  City: "Austin, TX",
+  Education: "Bachelor's degree in Mechanical Engineering",
+  Experience: "3 years of experience as a design engineer at an aerospace company",
+ },
+ {
+  Name: "Rachel Kim",
+  City: "Seattle, WA",
+  Education: "Bachelor's degree in Nursing",
+  Experience: "2 years of experience as a registered nurse at a hospital",
+ },
+ {
+  Name: "Kevin Nguyen",
+  City: "Boston, MA",
+  Education: "Master's degree in Education",
+  Experience: "5 years of experience as a high school teacher",
+ },
+ {
+  Name: "Jessica Rodriguez",
+  City: "Houston, TX",
+  Education: "Bachelor's degree in Journalism",
+  Experience: "3 years of experience as a journalist at a local newspaper",
+ },
+ {
+  Name: "Thomas Jackson",
+  City: "Denver, CO",
+  Education: "Master's degree in Public Health",
+  Experience: "7 years of experience as a public health researcher",
+ },
+ {
+  Name: "Thomas Jackson",
+  City: "Denver, CO",
+  Education: "Master's degree in Public Health",
+  Experience: "7 years of experience as a public health researcher",
+ },
+ {
+  Name: "Ashley Kim",
+  City: "Washington, DC",
+  Education: "Bachelor's degree in Political Science",
+  Experience: "4 years of experience as a legislative assistant for a member of Congress",
+ },
+ {
+  Name: "Christopher Lee",
+  City: "Philadelphia, PA",
+  Education: "Master's degree in Architecture",
+  Experience: "6 years of experience as an architect at an architecture firm.",
+ },
+
+
+ {
+    id: "j1",
+    title: "Front-End Software Developer",
+    company: "Spotify",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula scelerisque nulla, vel dignissim lectus blandit eget. Vestibulum at tellus ex. Curabitur tincidunt metus ut vulputate sodales. Nulla hendrerit, felis at finibus ornare, ex nisi pharetra mi, quis dictum ex libero nec purus. Ut hendrerit purus vitae tortor dignissim, nec dapibus tellus mollis. Curabitur sagittis arcu vel sem ultricies, id egestas orci luctus. Vestibulum non tincidunt tellus. Nunc at augue non diam elementum volutpat. Donec non massa non diam volutpat lobortis. Curabitur nec scelerisque leo. Fusce vitae lacus sem.",
+  },
+  {
+    id: "j2",
+    title: "Back-End Software Developer",
+    company: "Meta",
+    description:
+      "Duis vel blandit diam. Maecenas sapien nulla, scelerisque vitae mollis et, luctus at nulla. Sed quis magna at libero dignissim vehicula. Sed eu tristique ante. Curabitur egestas lectus ut dolor fermentum malesuada. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin convallis mollis leo sed rhoncus. Morbi consectetur ante et magna luctus finibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec et nibh odio. Morbi felis lectus, blandit quis eros ut, sagittis volutpat sapien. Donec nec augue porttitor, imperdiet elit vel, interdum nunc. Vestibulum sagittis sem at mauris aliquam aliquet. Phasellus quis pulvinar ligula. Integer eget lectus pretium, eleifend felis eu, tempus sapien. Suspendisse potenti.",
+  },
+  {
+    id: "j3",
+    title: "Professor",
+    company: "Concordia University",
+    description:
+      "Phasellus leo turpis, mattis eget hendrerit quis, ultricies a est. Phasellus vitae posuere felis. Donec at venenatis enim. Etiam euismod, felis sed pretium pellentesque, neque nibh luctus ligula, a varius urna augue nec lacus. Nulla blandit tortor neque, at laoreet nunc tempor vel. Nulla id ipsum ligula. Nulla facilisi. Quisque euismod, purus sit amet interdum accumsan, lorem diam fermentum risus, sed porttitor lectus nunc sed diam. Nullam vulputate nulla quam, ac consequat nibh ultricies nec.",
+  },
+  {
+    id: "j4",
+    title: "Fry Cook",
+    company: "McDonald's",
+    description:
+      "Donec eu scelerisque nisi, vulputate porta turpis. Donec mattis id lorem at interdum. Phasellus nec libero semper, accumsan quam id, placerat turpis. Proin vitae sodales tortor, non auctor dolor. Vestibulum volutpat leo id dui fringilla gravida. Suspendisse tristique tortor id imperdiet efficitur. Fusce quis lectus in risus pulvinar faucibus. Quisque nec tincidunt neque. Aenean dictum, nulla ac egestas venenatis, ante odio hendrerit quam, vel interdum lectus ipsum id orci. Nunc ligula ex, ornare sit amet leo et, suscipit laoreet odio. Pellentesque sit amet hendrerit ante. Nulla eget nunc risus. Cras sit amet diam in sem auctor pharetra."
+  },
+  {
+    id: "j5",
+    title: "Graphic Designer",
+    company: "Some Company",
+    description:
+      "Cras feugiat turpis sed sodales pellentesque. Nam nibh sapien, tempor eget venenatis a, viverra non dolor. Vestibulum malesuada nisl eu lacus fermentum, sed ullamcorper quam vehicula. Proin lacinia molestie urna, in commodo nunc efficitur tempus. Donec tristique lacinia placerat. Nam dignissim pellentesque orci in euismod. Nam nec libero a lectus pulvinar vestibulum nec eget nibh. Etiam felis purus, vulputate eu placerat et, convallis eget quam. Cras in lobortis augue, non semper velit. Maecenas pellentesque commodo mollis. Curabitur porttitor iaculis metus. Maecenas imperdiet nec felis at bibendum. Etiam eget sem vel massa eleifend tempus a nec enim. Morbi feugiat vel ipsum at aliquet."
+  },
+  {
+    id: "j6",
+    title: "Bitcoin Investor",
+    company: "Wall Street",
+    description:
+      "Mauris dapibus enim eget nulla pellentesque, sagittis pulvinar leo lacinia. Fusce tincidunt nisl massa, ac egestas nulla ornare sit amet. Nunc rhoncus auctor urna, eget vehicula ex venenatis ac. Aliquam vitae consequat metus, quis pretium orci. Nullam et metus sed tortor facilisis malesuada. Sed molestie lectus eu varius tristique. Pellentesque odio nulla, eleifend nec sodales ac, condimentum at metus. Morbi ut tellus consectetur, sollicitudin diam vel, tincidunt magna. Nulla vulputate, tortor sed varius molestie, magna dolor rutrum neque, eu blandit odio eros ut justo. Duis malesuada, dui nec porta gravida, ex erat tempus tellus, id interdum urna risus non neque."
+  },
+  {
+    id: "j7",
+    title: "Full-Stack Developer",
+    company: "Google",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula scelerisque nulla, vel dignissim lectus blandit eget. Vestibulum at tellus ex. Curabitur tincidunt metus ut vulputate sodales. Nulla hendrerit, felis at finibus ornare, ex nisi pharetra mi, quis dictum ex libero nec purus. Ut hendrerit purus vitae tortor dignissim, nec dapibus tellus mollis. Curabitur sagittis arcu vel sem ultricies, id egestas orci luctus. Vestibulum non tincidunt tellus. Nunc at augue non diam elementum volutpat. Donec non massa non diam volutpat lobortis. Curabitur nec scelerisque leo. Fusce vitae lacus sem."
+  },
+  {
+    id: "j8",
+    title: "Data Scientist",
+    company: "Pratt & Whitney",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula scelerisque nulla, vel dignissim lectus blandit eget. Vestibulum at tellus ex. Curabitur tincidunt metus ut vulputate sodales. Nulla hendrerit, felis at finibus ornare, ex nisi pharetra mi, quis dictum ex libero nec purus. Ut hendrerit purus vitae tortor dignissim, nec dapibus tellus mollis. Curabitur sagittis arcu vel sem ultricies, id egestas orci luctus. Vestibulum non tincidunt tellus. Nunc at augue non diam elementum volutpat. Donec non massa non diam volutpat lobortis. Curabitur nec scelerisque leo. Fusce vitae lacus sem."
+  },
+  {
+    id: "j9",
+    title: "Front-End Software Developer",
+    company: "Spotify",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula scelerisque nulla, vel dignissim lectus blandit eget. Vestibulum at tellus ex. Curabitur tincidunt metus ut vulputate sodales. Nulla hendrerit, felis at finibus ornare, ex nisi pharetra mi, quis dictum ex libero nec purus. Ut hendrerit purus vitae tortor dignissim, nec dapibus tellus mollis. Curabitur sagittis arcu vel sem ultricies, id egestas orci luctus. Vestibulum non tincidunt tellus. Nunc at augue non diam elementum volutpat. Donec non massa non diam volutpat lobortis. Curabitur nec scelerisque leo. Fusce vitae lacus sem."
+  },
+  {
+    id: "j10",
+    title: "Back-End Software Developer",
+    company: "Meta",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula scelerisque nulla, vel dignissim lectus blandit eget. Vestibulum at tellus ex. Curabitur tincidunt metus ut vulputate sodales. Nulla hendrerit, felis at finibus ornare, ex nisi pharetra mi, quis dictum ex libero nec purus. Ut hendrerit purus vitae tortor dignissim, nec dapibus tellus mollis. Curabitur sagittis arcu vel sem ultricies, id egestas orci luctus. Vestibulum non tincidunt tellus. Nunc at augue non diam elementum volutpat. Donec non massa non diam volutpat lobortis. Curabitur nec scelerisque leo. Fusce vitae lacus sem."
+  },
+  {
+    id: "j11",
+    title: "Professor",
+    company: "Concordia University",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula scelerisque nulla, vel dignissim lectus blandit eget. Vestibulum at tellus ex. Curabitur tincidunt metus ut vulputate sodales. Nulla hendrerit, felis at finibus ornare, ex nisi pharetra mi, quis dictum ex libero nec purus. Ut hendrerit purus vitae tortor dignissim, nec dapibus tellus mollis. Curabitur sagittis arcu vel sem ultricies, id egestas orci luctus. Vestibulum non tincidunt tellus. Nunc at augue non diam elementum volutpat. Donec non massa non diam volutpat lobortis. Curabitur nec scelerisque leo. Fusce vitae lacus sem."
+  },
+  {
+    id: "j12",
+    title: "Fry Cook",
+    company: "McDonald's",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula scelerisque nulla, vel dignissim lectus blandit eget. Vestibulum at tellus ex. Curabitur tincidunt metus ut vulputate sodales. Nulla hendrerit, felis at finibus ornare, ex nisi pharetra mi, quis dictum ex libero nec purus. Ut hendrerit purus vitae tortor dignissim, nec dapibus tellus mollis. Curabitur sagittis arcu vel sem ultricies, id egestas orci luctus. Vestibulum non tincidunt tellus. Nunc at augue non diam elementum volutpat. Donec non massa non diam volutpat lobortis. Curabitur nec scelerisque leo. Fusce vitae lacus sem."
+  },
+  {
+    id: "j13",
+    title: "Graphic Designer",
+    company: "Some Company",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula scelerisque nulla, vel dignissim lectus blandit eget. Vestibulum at tellus ex. Curabitur tincidunt metus ut vulputate sodales. Nulla hendrerit, felis at finibus ornare, ex nisi pharetra mi, quis dictum ex libero nec purus. Ut hendrerit purus vitae tortor dignissim, nec dapibus tellus mollis. Curabitur sagittis arcu vel sem ultricies, id egestas orci luctus. Vestibulum non tincidunt tellus. Nunc at augue non diam elementum volutpat. Donec non massa non diam volutpat lobortis. Curabitur nec scelerisque leo. Fusce vitae lacus sem."
+  },
+  {
+    id: "j14",
+    title: "Bitcoin Investor",
+    company: "Wall Street",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula scelerisque nulla, vel dignissim lectus blandit eget. Vestibulum at tellus ex. Curabitur tincidunt metus ut vulputate sodales. Nulla hendrerit, felis at finibus ornare, ex nisi pharetra mi, quis dictum ex libero nec purus. Ut hendrerit purus vitae tortor dignissim, nec dapibus tellus mollis. Curabitur sagittis arcu vel sem ultricies, id egestas orci luctus. Vestibulum non tincidunt tellus. Nunc at augue non diam elementum volutpat. Donec non massa non diam volutpat lobortis. Curabitur nec scelerisque leo. Fusce vitae lacus sem."
+  },
+  {
+    id: "j15",
+    title: "Full-Stack Developer",
+    company: "Google",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula scelerisque nulla, vel dignissim lectus blandit eget. Vestibulum at tellus ex. Curabitur tincidunt metus ut vulputate sodales. Nulla hendrerit, felis at finibus ornare, ex nisi pharetra mi, quis dictum ex libero nec purus. Ut hendrerit purus vitae tortor dignissim, nec dapibus tellus mollis. Curabitur sagittis arcu vel sem ultricies, id egestas orci luctus. Vestibulum non tincidunt tellus. Nunc at augue non diam elementum volutpat. Donec non massa non diam volutpat lobortis. Curabitur nec scelerisque leo. Fusce vitae lacus sem."
+  },
+  {
+    id: "j16",
+    title: "Data Scientist",
+    company: "Pratt & Whitney",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula scelerisque nulla, vel dignissim lectus blandit eget. Vestibulum at tellus ex. Curabitur tincidunt metus ut vulputate sodales. Nulla hendrerit, felis at finibus ornare, ex nisi pharetra mi, quis dictum ex libero nec purus. Ut hendrerit purus vitae tortor dignissim, nec dapibus tellus mollis. Curabitur sagittis arcu vel sem ultricies, id egestas orci luctus. Vestibulum non tincidunt tellus. Nunc at augue non diam elementum volutpat. Donec non massa non diam volutpat lobortis. Curabitur nec scelerisque leo. Fusce vitae lacus sem."
+  },
+];
+
+export function myJobs() {
+  return jobs;
+}
+
+export function findJob(id) {
+  return jobs.filter((job) => Object.values(job).indexOf(id) > -1)[0];
+}
