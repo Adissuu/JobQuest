f1b642e7d5984978f7a708cad5d3c95d90c9a223
The-Einzelganger <123901407+TitiMMM@users.noreply.github.com>
2023-02-28 13:37:26 -0500

    Modified the recruits item with specific styles

--- client/components/Recruits/RecruitsItem.js
+++ client/components/Recruits/RecruitsItem.js
@@ -1,10 +1,10 @@
 import styles from"../../styles/Jobs/jobs.module.css";
 
-function JobItem(props) {
+function RecruitItem(props) {
     return <div>
         <div className={styles.title}>{props.title}</div>
         <div className={styles.company}>{props.company}</div>
     </div>
 }
 
-export default JobItem;
+export default RecruitItem;
