ceaf0379c29a15a393eae2b6c6a90792bec7007b
Etienne Paquet <paquetetienne@gmail.com>
2023-02-28 20:29:37 -0500

    updated signin function to create short id

--- server/routes/user.js
+++ server/routes/user.js
@@ -9,6 +9,8 @@ const userRoutes = express.Router();
 const dbo = require("../db/conn");
 const applicant = require("../models/applicantModel");
 
+const shortId = require('shortid');
+
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
@@ -39,7 +41,7 @@ userRoutes.route("/user/:id").get(function (req, res) {
 
 
 
-// This section will help you create a new applicant.
+// This section will help you create a new user.
 userRoutes.route("/user/add").post(function (req, response) {
   let db_connect = dbo.getDb("JobQuest");
 
@@ -86,31 +88,56 @@ userRoutes.route("/:id").delete((req, response) => {
   });
 });
 
-//  signup function
 
-userRoutes.route("/signup").post(async function (req, res, next) {
+userRoutes.route("/signup").post(async function (req, res) {
 
     let db_connect = dbo.getDb();
     let myquery = { email: req.params.email};
 
+
+    console.log(req);
+
     db_connect.collection("Users").findOne(myquery, function(err, results)
+    //  check that the email isn't already used
     {
         if (err) throw err;
-        console.log(results == null);
+        let username = shortId.generate();
+        //console.log(results == null);
 
         if (results == null)
         {
             let myobj = {
                 email: req.body.email,
                 password: req.body.password,
-                role: req.body.role
+                role: req.body.role,
+                userId: username
             }
+            //console.log(myobj)
             db_connect.collection("Users").insertOne(myobj);
+
+            if (myobj.role == 0)
+            //  create new applicant
+            {
+                db_connect.collection("Applicants").insertOne(
+                {
+                    userId: username
+                }
+                );
+            }
+            else 
+            //  create new employer
+            {
+                db_connect.collection("Employers").insertOne(
+                    {
+                        userId: username
+                    }
+                )
+            }
             res.json("Signup successful. Enjoy your quest!")
         }
         else
         {
-            res.json("Email already exists. Select another email.");
+            res.json("Email is already used. Select another email.");
         }
     });
   })
@@ -125,6 +152,7 @@ userRoutes.route("/signin").post(async function (req, res) {
 
 
     db_connect.collection("Users").findOne(myquery, function(err, results)
+    //  check if there's a user with the corresponding email and password
     {
         if (err) throw err;
         console.log(results == null);
