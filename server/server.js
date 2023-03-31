f7c3f3e32b6dac9e61e0dc8d4113af6d897b4b98
Louis-Charles Marquis <lcmarquis42@gmail.com>
2023-03-31 12:27:53 -0400

    Add resume upload feature

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const jobPostingRoutes = require('./routes/jobPostings')
 const userRoutes = require('./routes/user')
 const applicantRoutes = require('./routes/applicants')
 const employerRoutes = require('./routes/employer')
+var multer  = require('multer')
 
 // express app
 const app = express()
@@ -36,3 +37,29 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((error) => {
     console.log(error)
   })
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+var upload = multer({ storage: storage })
+
+//store it to a “uploads” directory and make image accessible on web page
+app.use(express.static(__dirname + '/public'));
+//serve all files/folders inside of the 'uploads' directory
+// And make them accessible through http://localhost:3000/uploads
+app.use('/uploads', express.static('uploads'));
+
+app.post('/', upload.single('profile-file'), function (req, res, next) {
+  // req.file is the `profile-file` file
+  // req.body will hold the text fields, if there were any
+  console.log(JSON.stringify(req.file))
+  var response = '<a href="/">Home</a><br>'
+  response += "Files uploaded successfully.<br>"
+  response += `<img src="${req.file.path}" /><br>`
+  return res.send(response)
+})
